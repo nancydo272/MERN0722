b721bf98f1a5dbffbca3eb485b5c616db52b851c
Nancy Do <nancydo272@gmail.com>
2022-07-10 20:46:27 -0400

    first react

---
+++ assignments/Javascript/first-jsx/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <div className ="to-do-list">
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt Everst</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
