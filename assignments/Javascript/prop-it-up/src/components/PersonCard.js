b936ee21e35e95585158d00a874e0da7cebbc5aa
Nancy Do <nancydo272@gmail.com>
2022-07-14 13:52:32 -0400

    useState

--- assignments/Javascript/prop-it-up/src/components/PersonCard.js
+++ assignments/Javascript/prop-it-up/src/components/PersonCard.js
@@ -1,11 +1,14 @@
-import React from 'react'; 
+import React, {useState} from 'react'; 
 
 const PersonCard = (props) => {
+    const [age, setAge] = useState(props.age); 
+    
     return(
         <div>
             <h2>{ props.lastName }, {props.firstName} </h2>
-            <p> Age: { props.age }</p>
+            <p> Age: { age }</p>
             <p> Hair Color: { props.hairColor}</p>
+            <button onClick = {() => setAge(age + 1)} >Birthday Button for { props.firstName } {props.lastName } </button>
         </div>
     ); 
 }
