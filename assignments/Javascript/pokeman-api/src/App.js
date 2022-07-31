2b0381c589d177ebd0b8384528bad729c56d2675
Nancy Do <nancydo272@gmail.com>
2022-07-30 22:29:57 -0400

    pokeman

--- assignments/Javascript/pokeman-api/src/App.js
+++ assignments/Javascript/pokeman-api/src/App.js
@@ -1,34 +1,54 @@
 
 import './App.css';
-import { useState, useEffect } from 'react'; 
+import React,{ useState, useEffect } from 'react'; 
+import axios from 'axios'; 
 
 function App() {
   
   const [state, setState] = useState([]); 
 
   useEffect(()=>{
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
-    .then((result)=>{
-      console.log(result)
-      return result.json()
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+    .then((response) =>{
+      console.log(response.data.results)
+      setState(response.data.results)
     })
-    .then((res)=>{
-      console.log(res)
-      setState(res.results)
-    })
-    .catch((error)=>{
-      console.log("error")
-    })
-  }, [])
+    .catch((err)=> console.log(err))
+  }, []); 
   return (
     <div className="App">
-      {state.map((pokeman, index)=>{
-        return(
-          <div key={index}> {pokeman.name}</div>
-        )
-      })}
+      <ul>
+          {state.map((pokeman, index)=>(
+            <li key={index}> {pokeman.name}</li>
+          ))}
+        </ul>
     </div>
-  );
+  ); 
 }
-
 export default App;
+ //const [state, setState] = useState([]); 
+
+  // useEffect(()=>{
+  //   fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
+  //   .then((result)=>{
+  //     console.log(result)
+  //     return result.json()
+  //   })
+  //   .then((res)=>{
+  //     console.log(res)
+  //     setState(res.results)
+  //   })
+  //   .catch((error)=>{
+  //     console.log("error")
+  //   })
+  // }, [])
+  // return (
+  //   <div className="App">
+  //     {
+  //       state.map((pokeman, index)=>{
+  //       return(
+  //         <div key={index}> {pokeman.name}</div>
+  //       )
+  //     })}
+  //   </div>
+  // );
